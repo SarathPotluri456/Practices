cadece8bdd9d748485df34097d6f5a35ed8b8f7c
sys-38 <sys-38@sys-9-PC>
2018-02-21 17:14:29 +0530

    commit

--- src/Exception/Throws.java
+++ src/Exception/Throws.java
@@ -2,22 +2,21 @@ package Exception;
 
 public class Throws {
    
-	 void m1() throws InterruptedException
+	 void m1() throws InterruptedException 
 	 {
 		 Thread t1=new Thread();
-		 t1.sleep(100);
+		 t1.sleep(1000);
 	 }
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		Throws t1=new Throws();
-		try
-		{
+		System.out.println("fsflpf1");
+		
 			t1.m1();
-		}catch(Exception e)
-		{
-			System.out.println("fsflpf");
-		}
+		
+			System.out.println("fsflpf2");
+		
 
 	}
 
