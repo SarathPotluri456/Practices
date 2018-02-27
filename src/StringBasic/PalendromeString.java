a2831dc9ad520801aa05b5f28d65bb5ef4902861
sys-38 <sys-38@sys-9-PC>
2018-02-27 09:58:59 +0530

    New Commit

--- src/StringBasic/PalendromeString.java
+++ src/StringBasic/PalendromeString.java
@@ -7,6 +7,16 @@ public class PalendromeString {
 		System.out.println("Please Enter String to Check Either Plaemdrom r not:");
         String str=sc.next();
         String str1="";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+        	str1=str1+str.charAt(i);
+        }
+        if(str1.equals(str))
+        {
+        	System.out.println("Given String is Palendram");
+        }
+        else
+        	System.out.println("Not Palendram");
 
         
 	}
